359b59fdf4a701f85259a94f1c41f490623d7d5f
Hugo Belém <belem.hugo@hotmail.com>
2026-01-12 16:28:09 +0000

    refactor: unify search in main get endpoint

--- backend/modules/billing/urls.py
+++ backend/modules/billing/urls.py
@@ -5,14 +5,12 @@ from . import views
 
 urlpatterns = [
     path('receivables', views.ReceivablesEndpoint.as_view()),
-    path('receivables/', views.ReceivableSearchEndpoint.as_view()),
     path('receivables/<str:receivable_id>', views.ReceivablesEndpoint.as_view()),
     path(
         'receivables/<str:receivable_id>/settle',
         views.ReceivableSettleEndpoint.as_view()
     ),
     path('payables', views.PayablesEndpoint.as_view()),
-    path('payables/', views.PayableSearchEndpoint.as_view()),
     path('payables/<str:payable_id>', views.PayablesEndpoint.as_view()),
     path(
         'payables/<str:payable_id>/settle',
